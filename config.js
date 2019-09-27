0228e3418429ae0e79d95a7d730b0978cfbda484
alokshakya <alokshakyacode@gmail.com>
2019-09-27 12:05:21 +0530

    connected database successfully

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb+srv://test1_user:test1_pass@cluster0-dhu1i.mongodb.net/test?retryWrites=true&w=majority',
+    'facebook': {
+        clientId:'329999474206572',
+        clientSecret:'56503eed5402bd290d95be895f5ca89d'
+    }
+}
